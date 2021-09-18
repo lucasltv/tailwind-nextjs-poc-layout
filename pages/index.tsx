d9e632fe9f9f31e837b224bdf62b3ba07d8f4326
Lucas Vieira <lucas@devplace.com.br>
2021-09-18 00:37:50 -0300

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -197,7 +197,9 @@ export default function Example() {
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {/* Replace with your content */}
           <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-screen">
+              TESTE
+            </div>
           </div>
           {/* /End replace */}
         </div>
